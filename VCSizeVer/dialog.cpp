64bfd3f241e75cad52fa28a89f03e179d27cccd3
= <=>
2014-11-05 23:48:53 +1100

    half version

--- VCSizeVer/dialog.cpp
+++ VCSizeVer/dialog.cpp
@@ -3,6 +3,12 @@
 Dialog::Dialog(QWidget *parent)
     : QDialog(parent)
 {
+    height=100;
+    width=200;
+
+    secret=new QImage(width,height,QImage::Format_RGBA8888);
+    s1=new QImage(width*2,height*2,QImage::Format_RGBA8888);
+    s2=new QImage(width*2,height*2,QImage::Format_RGBA8888);
 }
 
 Dialog::~Dialog()
--- VCSizeVer/dialog.h
+++ VCSizeVer/dialog.h
@@ -2,7 +2,7 @@
 #define DIALOG_H
 
 #include <QDialog>
-
+#include <QImage>
 class Dialog : public QDialog
 {
     Q_OBJECT
@@ -10,6 +10,26 @@ class Dialog : public QDialog
 public:
     Dialog(QWidget *parent = 0);
     ~Dialog();
+
+private:
+    int height;
+    int width;
+
+    QImage* secret;
+    QImage* s1;
+    QImage* s2;
+
+    void generateS1();
+    void generateSecret();
+    void generateS2();
+    void saveAll();
+
+    void topHor(QImage*,int,int);
+    void botHor(QImage*,int,int);
+    void lefVer(QImage*,int,int);
+    void rigVer(QImage*,int,int);
+    void lefRig(QImage*,int,int);
+    void rigLef(QImage*,int,int);
 };
 
 #endif // DIALOG_H
