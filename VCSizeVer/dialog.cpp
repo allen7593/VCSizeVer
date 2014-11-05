0936b1ce088cf5355de4a98a2f79c48a56cbd63a
= <=>
2014-11-05 23:49:24 +1100

    half version

--- VCSizeVer/dialog.cpp
+++ VCSizeVer/dialog.cpp
@@ -9,6 +9,8 @@ Dialog::Dialog(QWidget *parent)
     secret=new QImage(width,height,QImage::Format_RGBA8888);
     s1=new QImage(width*2,height*2,QImage::Format_RGBA8888);
     s2=new QImage(width*2,height*2,QImage::Format_RGBA8888);
+
+
 }
 
 Dialog::~Dialog()
